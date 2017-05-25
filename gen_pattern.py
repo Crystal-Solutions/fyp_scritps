54bbdf55b0f8abe21030fe1df87c8e0c204788e2
shanika <shanika.13@cse.mrt.ac.lk>
2017-05-25 21:59:23 +0530

    Add w2v pattern generation for 3word and 5word windows

--- gen_pattern.py
+++ gen_pattern.py
@@ -5,31 +5,116 @@ Created on Thu May 25 20:31:00 2017
 @author: Shanika Ediriweera
 """
 
+pattern_file = open('generated_pattern.txt','w')
+
 """
 generate pattern for word2vec unigrams for 1 word window
 """
 def gen_w2v_unigram_single():   
-    with open('generated_pattern.txt','w') as f:
-        for i in range(7,307):
-            line = 'U:w2v-1 X=%X[ 0,'+ str(i) + ']'
-            f.write(line+"\n")
-            print(line)
+    for i in range(7,307):
+        line = 'U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        pattern_file.write(line+"\n")
+        print(line)
             
     
 """
 generate pattern for word2vec unigrams for 1 word window commented
 """
 def gen_w2v_unigram_single_commented():   
-    with open('generated_pattern.txt','w') as f:
-        for i in range(7,307):
-            line = '#U:w2v-1 X=%X[ 0,'+ str(i) + ']'
-            f.write(line+"\n")
-            print(line)
+    for i in range(7,307):
+        line = '#U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        pattern_file.write(line+"\n")
+        print(line)
+        
 
+"""
+generate pattern for word2vec unigrams for 3 word window
+"""
+def gen_w2v_unigram_3window():   
+    for i in range(7,307):
+        line1 = 'U:w2v-f' + str(i-6) + '-1 L=%X[-1,' + str(i) + ']'
+        line2 = 'U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        line3 = 'U:w2v-f' + str(i-6) + '-1 R=%X[ 1,' + str(i) + ']'
+        pattern_file.write(line1 + "\n" + line2 + "\n" + line3 + "\n\n")
+        print(line1 + "\n" + line2 + "\n" + line3+ "\n")
+
+
+"""
+generate pattern for word2vec unigrams for 3 word window commented
+"""
+def gen_w2v_unigram_3window_commented():   
+    for i in range(7,307):
+        line1 = '#U:w2v-f' + str(i-6) + '-1 L=%X[-1,' + str(i) + ']'
+        line2 = '#U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        line3 = '#U:w2v-f' + str(i-6) + '-1 R=%X[ 1,' + str(i) + ']'
+        pattern_file.write(line1 + "\n" + line2 + "\n" + line3 + "\n")
+        print(line1 + "\n" + line2 + "\n" + line3+ "\n")
+
+
+"""
+generate pattern for word2vec unigrams for 5 word window
+"""
+def gen_w2v_unigram_5window():   
+    for i in range(7,307):
+        line1 = 'U:w2v-f' + str(i-6) + '-1LL=%X[-2,' + str(i) + ']'
+        line2 = 'U:w2v-f' + str(i-6) + '-1 L=%X[-1,' + str(i) + ']'
+        line3 = 'U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        line4 = 'U:w2v-f' + str(i-6) + '-1 R=%X[ 1,' + str(i) + ']'
+        line5 = 'U:w2v-f' + str(i-6) + '-1RR=%X[ 2,' + str(i) + ']'
+        pattern_file.write(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 + "\n\n")
+        print(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 +"\n")
+        
+        
+"""
+generate pattern for word2vec unigrams for 5 word window commented
+"""
+def gen_w2v_unigram_5window_commented():   
+    for i in range(7,307):
+        line1 = '#U:w2v-f' + str(i-6) + '-1LL=%X[-2,' + str(i) + ']'
+        line2 = '#U:w2v-f' + str(i-6) + '-1 L=%X[-1,' + str(i) + ']'
+        line3 = '#U:w2v-f' + str(i-6) + '-1 X=%X[ 0,' + str(i) + ']'
+        line4 = '#U:w2v-f' + str(i-6) + '-1 R=%X[ 1,' + str(i) + ']'
+        line5 = '#U:w2v-f' + str(i-6) + '-1RR=%X[ 2,' + str(i) + ']'
+        pattern_file.write(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 + "\n\n")
+        print(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 +"\n")
+        
+        
 """
 generate pattern
 """
 def generate_pattern():
-    gen_w2v_unigram_single()
+    pattern_file.write("#Word 2 vec features\n\n")
+    #gen_w2v_unigram_single()
+    #gen_w2v_unigram_single_commented()
+    #gen_w2v_unigram_3window()
+    gen_w2v_unigram_5window()
+    
     
 generate_pattern()
+pattern_file.close()
+
+'''
+U:w2v-1 X=%X[ 0,0]
+
+U:wrd-1 L=%X[-1,0]
+U:wrd-1 X=%X[ 0,0]
+U:wrd-1 R=%X[ 1,0]
+
+U:wrd-1LL=%X[-2,0]
+U:wrd-1 L=%X[-1,0]
+U:wrd-1 X=%X[ 0,0]
+U:wrd-1 R=%X[ 1,0]
+U:wrd-1RR=%X[ 2,0]
+
+U:wrd-2 L=%X[-1,0]/%X[ 0,0]
+U:wrd-2 R=%X[ 0,0]/%X[ 1,0]
+
+U06:%x[-2,0]/%x[-1,0]
+U07:%x[-1,0]/%x[0,0]
+U08:%x[0,0]/%x[1,0]
+U09:%x[1,0]/%x[2,0]
+
+U10:%x[-2,0]/%x[-1,0]/%x[0,0]
+U11:%x[-1,0]/%x[0,0]/%x[1,0]
+U12:%x[0,0]/%x[1,0]/%x[2,0]
+'''
